d404b6f9d8179338bea9a890ccd4cf32131acaf3
solufa <solufa2020@gmail.com>
2025-05-14 19:23:00 +0900

    first commit

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  basePath:
+    process.env.GITHUB_REPOSITORY !== undefined
+      ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}`
+      : '',
+  output: 'export',
+  trailingSlash: true,
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+};
+
+export default nextConfig;
