cf95a555c5f0fc62179acc7ef328b2b6bd97d546
solufa <solufa2020@gmail.com>
2025-06-06 23:45:03 +0900

    chore: remove better-auth

--- src/lib/auth.ts
+++
@@ -1,7 +0,0 @@
-import { betterAuth } from 'better-auth';
-import { prismaAdapter } from 'better-auth/adapters/prisma';
-import { prismaClient } from 'server/lib/prismaClient';
-
-export const auth = betterAuth({
-  database: prismaAdapter(prismaClient, { provider: 'postgresql' }),
-});
